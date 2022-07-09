19a6a1af38d40579ae192e8e6edfbce6f4bca236
llLeo999 <107154510+llLeo999@users.noreply.github.com>
2022-07-09 13:29:18 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+background(24, 6, 51);
+
+// arms
+strokeWeight(10);
+stroke(87, 53, 8);
+// left1
+line(250, 200, 341, 254);
+// left2
+line(341, 254, 546, -54);
+// right1
+line(150, 200, 91, 138);
+// right2
+line(91, 138, 8, 218);
+
+// simple snowman
+noStroke();
+fill(255, 255, 255);
+ellipse(200, 300, 150, 150);
+ellipse(200, 200, 100, 100);
+ellipse(200, 120, 75, 75);
+
+
+// mouth
+fill(0);
+ellipse(213, 135, 165, 14);
+
+// eyes
+fill(255, 0, 0);
+
+ellipse(227, 113, 22, 22);
+
+//nose
+fill(255, 149, 0);
+triangle(200, 125, 300, 30, 200, 115);
+fill(255, 0, 0);
+ellipse(191, 113, 22, 22);
